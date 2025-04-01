20b2438b669db851869960bad9d974596ff38b4c
Juan Monroy <juan.monroy@catolica.edu.sv>
2025-04-01 17:17:36 -0600

    Modelos y clase context

--- L02P02_2022AA650_2022MA653/Models/ClaseContext.cs
+++ L02P02_2022AA650_2022MA653/Models/ClaseContext.cs
@@ -4,9 +4,13 @@ namespace L02P02_2022AA650_2022MA653.Models
 {
     public class ClaseContext: DbContext
     {
-        public ClaseContext(DbContextOptions<ClaseContext> options) : base(options)
-        {
-
-        }
+        public ClaseContext(DbContextOptions<ClaseContext> options) : base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<PedidoEncabezado> PedidoEncabezados { get; set; }
+        public DbSet<PedidoDetalle> PedidoDetalles { get; set; }
+        public DbSet<ComentarioLibro> ComentariosLibros { get; set; }
     }
 }
