eab7e950815a67d8f176466ceb23a6ff31695dae
Rod <oscar.aguilar1@catolica.edu.sv>
2025-04-01 16:54:04 -0600

    Add project files.

---
+++ L02P02_2022AA650_2022MA653/Models/ClaseContext.cs
@@ -0,0 +1,6 @@
+﻿namespace L02P02_2022AA650_2022MA653.Models
+{
+    public class ClaseContext
+    {
+    }
+}
---
+++ L02P02_2022AA650_2022MA653/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022AA650_2022MA653.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
